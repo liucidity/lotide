e90c79dd1b57a5da3bf8643b3a970de9fa2ee1f3
Travis <travisliu708@gmail.com>
2022-08-22 13:15:36 -0700

    created a more verbose assertion to compare two values and prints success or failure message

---
+++ assertEqual.js
@@ -0,0 +1,20 @@
+//Function implementation
+const assertEqual = function(actual, expected) {
+  console.log(actual === expected ?
+    `✅✅✅ Assertion Passed: [${actual}] === [${expected}]` :
+    `🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+
+};
+
+//Test Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+
+//Comparing identical strings
+assertEqual('Travis', 'Travis');
+//Comparing non-identical strings
+assertEqual('iuewahfjkda','aufheuiwahfiukd');
+//Comparing identical numbers
+assertEqual(22,22);
+//Comparing non-identical numbers
+assertEqual(28,32);
