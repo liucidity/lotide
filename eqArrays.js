20f85f78af63de4f6278532c8abce98b3d086e6d
Travis <travisliu708@gmail.com>
2022-08-26 14:05:32 -0700

    removed test cases

--- eqArrays.js
+++ eqArrays.js
@@ -27,15 +27,15 @@ const eqArrays = function(arr1,arr2) {
 
 module.exports = {eqArrays:eqArrays};
 
-eqArrays([[2, 3], [4]], [[2, 3], [4]], true); // => true
+// eqArrays([[2, 3], [4]], [[2, 3], [4]], true); // => true
 
-eqArrays([[2, 3], [4]], [[2, 3], [4, 5]],false);// => false
-eqArrays([[2, 3], [4]], [[2, 3], 4], false); // => false
-assertEqual.assertEqual(eqArrays([[[[2, 3]], [4]]], [[[[2, 3]], [4]]]), true);
-assertEqual.assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]),false);
-assertEqual.assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]),false);
+// eqArrays([[2, 3], [4]], [[2, 3], [4, 5]],false);// => false
+// eqArrays([[2, 3], [4]], [[2, 3], 4], false); // => false
+// assertEqual.assertEqual(eqArrays([[[[2, 3]], [4]]], [[[[2, 3]], [4]]]), true);
+// assertEqual.assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]),false);
+// assertEqual.assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]),false);
 
-assertEqual.assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+// assertEqual.assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 // assertEqual.assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
 // assertEqual.assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
