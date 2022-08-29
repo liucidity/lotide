5182a539ab2e5f8a38b12c2e250b8eaa3c737976
Travis <travisliu708@gmail.com>
2022-08-28 23:13:48 -0700

    refactored middle.js as module and moved tests to file

--- middle.js
+++ middle.js
@@ -14,8 +14,4 @@ const middle = function(array) {
   return [array[middleIndex]];
 };
 
-//Tests
-assertArraysEqual.assertArraysEqual(middle([1,2,3,4,5,6,7,8,9]), [5]);
-assertArraysEqual.assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
-assertArraysEqual.assertArraysEqual(middle([2,4]), []);
-assertArraysEqual.assertArraysEqual(middle([1]), []);
+module.exports = middle;
